33d0543e286197c87687a16d53d4fa3c2d228a0e
TomoyaWaniko <alligatorfree12@gmail.com>
2024-06-14 13:47:44 +0900

    update

---
+++ src/main/java/org/example/springudemy1/SpringUdemy1Application.java
@@ -0,0 +1,14 @@
+package org.example.springudemy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUdemy1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUdemy1Application.class, args);
+        System.out.println("this");
+    }
+
+}
---
+++ src/test/java/org/example/springudemy1/SpringUdemy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springudemy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUdemy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
