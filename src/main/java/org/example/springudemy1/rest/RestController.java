33953af5685465892b337f165664ca4dc48b73ca
TomoyaWaniko <alligatorfree12@gmail.com>
2024-06-18 13:17:47 +0900

    update

--- src/main/java/org/example/springudemy1/rest/RestController.java
+++ src/main/java/org/example/springudemy1/rest/RestController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-//    expose "/" that returns "Hello World"
-
+    //    expose '/' that returns "Hello World"
     @GetMapping("/")
     public String sayHello() {
-        return "Hello Wolrd!";
+        return "HELLO WORLD!";
     }
 }
